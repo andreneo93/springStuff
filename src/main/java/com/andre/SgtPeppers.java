d137cfb3197011a9dd0f7634d2b4478ec4c045af
andre <andre@sweetHome>
2015-10-29 19:56:33 +0100

    Base project sources Chapter 2

---
+++ src/main/java/com/andre/SgtPeppers.java
@@ -0,0 +1,14 @@
+package soundsystem;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SgtPeppers implements CompactDisc {
+	private String title = "Sgt. Pepper's lonely heart club bund";
+	private String name = "The beatles";
+
+
+	public void play() {
+		System.out.println("Playing " + title + " by " + name);
+	}
+}
